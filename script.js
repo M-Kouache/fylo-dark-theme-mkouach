7015b1471100abfd8d3a0379af1d80acbc875713
M-Kouache <mkouache27@gmail.com>
2020-10-13 02:41:27 +0000

    slid-menu

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let btn = document.getElementById("slid-btn");
+let nav = document.getElementById("slid-menu");
+
+
+btn.addEventListener("click", function () {
+    nav.classList.toggle("active");
+    btn.classList.toggle("active");
+    
+})
+
+
+
+
