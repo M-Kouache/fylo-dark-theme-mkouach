8c7df12f18516e844c86678ea2537eb162c8aaf2
M-Kouache <mkouache27@gmail.com>
2020-10-13 20:32:55 +0000

    singUp-form

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+const signup= document.querySelectorAll(".signup");
 
-let btn = document.getElementById("slid-btn");
-let nav = document.getElementById("slid-menu");
+const pup = document.getElementById("sign-pup");
+const btn = document.getElementById("slid-btn");
+const nav = document.getElementById("slid-menu");
+const close_signup_btn = document.getElementById("close-signup-btn");
+
+for(let i=0;i<signup.length;i++){
+        
+    signup[i].addEventListener("click",function () {
+
+        pup.classList.add("active");
+
+    });
+}
+
+close_signup_btn.addEventListener("click",function () {
+
+    pup.classList.remove("active")
+
+});
 
 
 btn.addEventListener("click", function () {
     nav.classList.toggle("active");
     btn.classList.toggle("active");
     
-})
+});
+
+
+
+
 
 
 
