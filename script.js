bcb13e7810693645ca169684fb9533df5c97fc50
M-Kouache <mkouache27@gmail.com>
2020-10-14 00:22:13 +0000

    autowrite header

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const signup= document.querySelectorAll(".signup");
+const viking = document.getElementById("auto-header");
 
 const pup = document.getElementById("sign-pup");
 const btn = document.getElementById("slid-btn");
 const nav = document.getElementById("slid-menu");
 const close_signup_btn = document.getElementById("close-signup-btn");
+
+const text = "All your files in once secure location,   accessible anywhere.";
+let index = 0;
+
+
+function autoWrite () {
+
+    viking.textContent = text.slice(0,index);
+
+    index++;
+
+}
+
+setInterval(autoWrite,80);
 
 for(let i=0;i<signup.length;i++){
         
@@ -13,6 +28,9 @@ for(let i=0;i<signup.length;i++){
 
     });
 }
+
+
+
 
 close_signup_btn.addEventListener("click",function () {
 
